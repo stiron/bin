30f9471466ba22a2adc07e697205d1e6ce2c6a73
Tamas MOLNAR <stiron@gmail.com>
2014-02-21 19:27:31 +0100

    Added some files from my chef-repo because of a Berkshelf bug to the Ruby version as well

--- clean-home.rb
+++ clean-home.rb
@@ -5,4 +5,5 @@ require 'fileutils'
 FileUtils.cd(ENV["HOME"]) do
   FileUtils.rm Dir.glob('.goutputstream-*')
   FileUtils.rm Dir.glob('libpeerconnection.log')
+  FileUtils.rm Dir.glob('chef-repo/d2014*')
 end
