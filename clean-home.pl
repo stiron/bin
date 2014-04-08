39cdbf46cf14fba97897f6ad1e0e40f6c33bf23c
Tamas MOLNAR <stiron@gmail.com>
2014-04-08 21:06:34 +0200

    Added the Perl5 version of my clean-home script

---
+++ clean-home.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $home_dir = $ENV{"HOME"};
+
+unlink glob "$home_dir/*debuglog.txt";
+unlink glob "$home_dir/.goutputstream-*";
+unlink glob "$home_dir/libpeerconnection*";
