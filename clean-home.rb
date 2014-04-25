f648f4a5ea1d87c45e95ea8c4eb497c735524a15
Tamas MOLNAR <stiron@gmail.com>
2014-04-25 19:19:48 +0200

    Added a new thing to clean

--- clean-home.rb
+++ clean-home.rb
@@ -5,6 +5,7 @@ require 'fileutils'
 FileUtils.cd(ENV["HOME"]) do
   FileUtils.rm Dir.glob('.goutputstream-*')
   FileUtils.rm Dir.glob('libpeerconnection.log')
+  FileUtils.rm Dir.golob('*debuglog.txt')
   FileUtils.rmdir Dir.glob('chef-repo/d2014*')
   FileUtils.rmdir Dir.glob('d2014*')
 end
