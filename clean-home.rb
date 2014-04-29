3f73b43b0040c5b061c4491c3607d6a13b25723d
Tamas MOLNAR <stiron@gmail.com>
2014-04-29 20:40:50 +0200

    Fixed a typo

--- clean-home.rb
+++ clean-home.rb
@@ -5,7 +5,7 @@ require 'fileutils'
 FileUtils.cd(ENV["HOME"]) do
   FileUtils.rm Dir.glob('.goutputstream-*')
   FileUtils.rm Dir.glob('libpeerconnection.log')
-  FileUtils.rm Dir.golob('*debuglog.txt')
+  FileUtils.rm Dir.glob('*debuglog.txt')
   FileUtils.rmdir Dir.glob('chef-repo/d2014*')
   FileUtils.rmdir Dir.glob('d2014*')
 end
