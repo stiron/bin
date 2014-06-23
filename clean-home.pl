a8cc705e9cd706b8c3c5a6ac9dafd0ad5cf440eb
Tamas MOLNAR <stiron@gmail.com>
2014-06-23 20:58:30 +0200

    Added a new line of unlink

--- clean-home.pl
+++ clean-home.pl
@@ -8,3 +8,4 @@ my $home_dir = $ENV{"HOME"};
 unlink glob "$home_dir/*debuglog.txt";
 unlink glob "$home_dir/.goutputstream-*";
 unlink glob "$home_dir/libpeerconnection*";
+unlink "C:\\nppdf32Log\\debuglog.txt";
