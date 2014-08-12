76c2b9e6bf9e62b29ae4c6e877e947a7c0041915
Tamas MOLNAR <stiron@gmail.com>
2014-08-12 21:17:16 +0200

    New module file

---
+++ CleanHome.pm
@@ -0,0 +1,44 @@
+package CleanHome;
+
+=head1 OBJECT METHODS
+
+=over 4
+
+=item new()
+
+  Creates a new object instance
+
+=cut
+
+sub new {
+  my $this = shift;
+  my $class = ref($this) || $this;
+  my $self = bless {@_}, $class;
+  return $self;
+}
+
+=item del_trash()
+
+  Deletes an array of files
+
+Parameters:
+
+  param del_arr : (array) list of files to be deleted
+  return        : the deleted file name
+
+=cut
+
+sub del_trash {
+  my $self = shift;
+  my @files_to_del = @_;
+  for my $to_delete ( @files_to_del ) {
+    unlink $to_delete;
+    return $to_delete;
+  }
+}
+
+1;
+
+=back
+
+=cut
