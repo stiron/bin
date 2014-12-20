e17f84109b06f05a14db27136b5ac102c00325c6
Tamas MOLNAR <stiron@gmail.com>
2014-12-20 13:59:27 +0100

    I made it more "script-like"

--- clean-home.pl
+++ clean-home.pl
@@ -3,24 +3,52 @@
 use strict;
 use warnings;
 
-use FindBin qw($Bin);
-use lib "$Bin/lib";
+{
+  package CleanHome;
 
-use CleanHome;
+  sub new {
+    my $this = shift;
+    my $class = ref($this) || $this;
+    my $self = bless {@_}, $class;
+    return $self;
+  }
 
-my @del_arr;
-my $home_dir  = $ENV{"HOME"};
-my @debug     = glob "$home_dir/*debuglog.txt";
-my @gout      = glob "$home_dir/.goutputstream-*";
-my @libp      = glob "$home_dir/libpeerconnection*";
-my $debug     = $home_dir . '/C:\nppdf32Log\debuglog.txt';
+  sub delete_files {
+    my $self = shift;
+    my @files_to_del = @_;
+    for my $to_delete ( @files_to_del ) {
+      unlink $to_delete;
+    }
+  }
 
-push @del_arr, @debug;
-push @del_arr, @gout;
-push @del_arr, @libp;
-push @del_arr, $debug if -e $debug;
-
-if ( @del_arr > 0 ) {
-  my $delete = CleanHome->new;
-  $delete->del_trash( @del_arr );
+  1;
 }
+
+sub create_and_delete_file_list {
+  my @del_arr;
+  my $home_dir  = $ENV{"HOME"};
+  my @debug     = glob "$home_dir/*debuglog.txt";
+  my @gout      = glob "$home_dir/.goutputstream-*";
+  my @libp      = glob "$home_dir/libpeerconnection*";
+  my $debug     = $home_dir . '/C:\nppdf32Log\debuglog.txt';
+
+  push @del_arr, @debug;
+  push @del_arr, @gout;
+  push @del_arr, @libp;
+  push @del_arr, $debug if -e $debug;
+
+  if ( @del_arr > 0 ) {
+    my $delete = CleanHome->new;
+    $delete->delete_files( @del_arr );
+  } else {
+    die "There is nothing to clean at the moment!\n";
+  }
+
+  return 1;
+}
+
+sub main {
+  create_and_delete_file_list();
+}
+
+main();
