1c6539adeb4cd8606782948216406f922978eee8
Tamas MOLNAR <stiron@gmail.com>
2014-12-27 15:58:06 +0100

    Using perltidy on the file

--- clean-home.pl
+++ clean-home.pl
@@ -4,57 +4,59 @@ use strict;
 use warnings;
 
 {
-  package CleanMyHome;
 
-  sub new {
-    my $this = shift;
-    my $class = ref($this) || $this;
-    my $self = bless {@_}, $class;
-    return $self;
-  }
+    package CleanMyHome;
 
-  sub delete_files {
-    my $self = shift;
-    my @files_to_del = @_;
-    for my $to_delete ( @files_to_del ) {
-      unlink $to_delete;
+    sub new {
+        my $this  = shift;
+        my $class = ref($this) || $this;
+        my $self  = bless {@_}, $class;
+        return $self;
     }
-  }
 
-  1;
+    sub delete_files {
+        my $self         = shift;
+        my @files_to_del = @_;
+        for my $to_delete (@files_to_del) {
+            unlink $to_delete;
+        }
+    }
+
+    1;
 }
 
 sub create_file_list {
-  my @del_arr;
-  my $home_dir  = $ENV{"HOME"};
-  my @debug     = glob "$home_dir/*debuglog.txt";
-  my @gout      = glob "$home_dir/.goutputstream-*";
-  my @libp      = glob "$home_dir/libpeerconnection*";
-  my $debug     = $home_dir . '/C:\nppdf32Log\debuglog.txt';
+    my @del_arr;
+    my $home_dir = $ENV{"HOME"};
+    my @debug    = glob "$home_dir/*debuglog.txt";
+    my @gout     = glob "$home_dir/.goutputstream-*";
+    my @libp     = glob "$home_dir/libpeerconnection*";
+    my $debug    = $home_dir . '/C:\nppdf32Log\debuglog.txt';
 
-  push @del_arr, @debug;
-  push @del_arr, @gout;
-  push @del_arr, @libp;
-  push @del_arr, $debug if -e $debug;
+    push @del_arr, @debug;
+    push @del_arr, @gout;
+    push @del_arr, @libp;
+    push @del_arr, $debug if -e $debug;
 
-  return \@del_arr;
+    return \@del_arr;
 }
 
 sub delete_files {
-  my @del_arr = @_;
-  if ( @del_arr > 0 ) {
-    my $delete = CleanMyHome->new;
-    $delete->delete_files( @del_arr );
-  } else {
-    die "There is nothing to clean at the moment!\n";
-  }
+    my @del_arr = @_;
+    if ( @del_arr > 0 ) {
+        my $delete = CleanMyHome->new;
+        $delete->delete_files(@del_arr);
+    }
+    else {
+        die "There is nothing to clean at the moment!\n";
+    }
 
-  return 1;
+    return 1;
 }
 
 sub main {
-  my $del_arr = create_file_list();
-  delete_files( @{$del_arr} );
+    my $del_arr = create_file_list();
+    delete_files( @{$del_arr} );
 }
 
 main();
