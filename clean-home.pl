6a3baae276100086ba5ad47462cbc91bf6b84d6c
Tamas Molnar <stiron@gmail.com>
2015-05-26 20:06:03 +0200

    Warn if file cannot be deleted

--- clean-home.pl
+++ clean-home.pl
@@ -3,10 +3,10 @@
 use strict;
 use warnings;
 
-sub unlink_them {
+sub unlink_files {
     my @files_to_del = @_;
     for my $to_delete (@files_to_del) {
-        unlink $to_delete;
+        unlink $to_delete or warn "Cannot unlink $to_delete: $!\n";
     }
 }
 
@@ -19,7 +19,7 @@ sub create_file_list {
 sub delete_files {
     my @del_arr = @_;
     if ( @del_arr > 0 ) {
-        unlink_them(@del_arr);
+        unlink_files(@del_arr);
     }
     else {
         exit 0;
@@ -29,7 +29,7 @@ sub delete_files {
 
 sub main {
     my $del_arr = create_file_list();
-    delete_files( @$del_arr );
+    delete_files(@$del_arr);
 }
 
 main();
