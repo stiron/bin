1d2dd4f88e5e5ae8ad740267329d99cedd6010f5
Tamas Molnar <stiron@gmail.com>
2015-07-07 21:32:07 +0200

    Improved subprocess runs with IPC::Run3

--- android-mount.pl
+++ android-mount.pl
@@ -2,52 +2,75 @@
 
 use strict;
 use warnings;
+use Data::Dumper;
+use IPC::Run3;
 
 check_installed();
 
-if ( ! $ARGV[0] ) {
-  exit_error();
+sub run_cmd {
+    my @cmd = @_;
+    my $stderr;
+    my $stdout;
+
+    run3( \@cmd, undef, \$stdout, \$stderr );
+    my $exit = $?;
+
+    my %retval = (
+        stdout => $stdout,
+        stderr => $stderr,
+        exit   => $exit,
+    );
+
+    return \%retval;
+}
+
+if ( !$ARGV[0] ) {
+    exit_error();
 }
 my $action = $ARGV[0];
 
 if ( $action ne "mount" and $action ne "umount" ) {
-  exit_error();
-} elsif ( $action eq "mount" ) {
-  mount_fs();
-} elsif ( $action eq "umount" ) {
-  umount_fs();
+    exit_error();
+}
+elsif ( $action eq "mount" ) {
+    mount_fs();
+}
+elsif ( $action eq "umount" ) {
+    umount_fs();
 }
 
 sub mount_fs {
-  qx(/usr/bin/go-mtpfs /media/MyAndroid);
-  my $check_mount = qx(/bin/mount |grep /media/MyAndroid);
-  if ( length( $check_mount ) > 0 ) {
-    print "The filesystem has been mounted\n";
-  } else {
+    my @cmd  = qw(/usr/bin/go-mtpfs /media/MyAndroid);
+    my $data = run_cmd(@cmd);
+    if ( $data->{exit} != 0 ) {
+        die("Command run was not successful: $data->{stderr}\n");
+    }
     print "The filesystem could not be mounted\n";
-  }
+    return 1;
 }
 
 sub umount_fs {
-  qx(/bin/fusermount -u /media/MyAndroid);
-  my $check_umount = qx(/bin/mount |grep /media/MyAndroid);
-  if ( length( $check_umount ) == 0 ) {
-    print "The filesystem has been umounted\n";
-  } else {
-    print "The filesystem could not be umounted\n";
-  }
+    my @cmd = qw(/bin/fusermount -u /media/MyAndroid);
+    my $data = run_cmd(@cmd);
+    my $check_umount = qx(/bin/mount |grep /media/MyAndroid);
+    if ( $data->{exit} != 0 ) {
+        die("Command run was not successful: $data->{stderr}\n");
+    }
+    return 1;
 }
 
 sub exit_error {
-  print "Usage: $0 [mount|umount]\n";
-  exit 1;
+    print "Usage: $0 [mount|umount]\n";
+    exit 1;
 }
 
 sub check_installed {
-  my $cmd = qx(which go-mtpfs);
-  if ( length( $cmd ) == 0 ) {
-    print "Please install the go-mtpfs command before!\n";
-    print "http://www.webupd8.org/2012/12/how-to-mount-android-40-ubuntu-go-mtpfs.html\n";
-    exit 1;
-  }
+    my @cmd  = qw(which go-mtpfs);
+    my $data = run_cmd(@cmd);
+    if ( $data->{exit} != 0 ) {
+        print "Please install the go-mtpfs command before!\n";
+        print
+"http://www.webupd8.org/2012/12/how-to-mount-android-40-ubuntu-go-mtpfs.html\n";
+        exit 1;
+    }
 }
